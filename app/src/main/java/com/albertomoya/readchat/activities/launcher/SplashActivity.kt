9c26e807939d9801456fa07fad84d1d83b39e8ea
Alberto Moya <moya.alonso.alberto@gmail.com>
2020-09-14 20:19:57 +0200

    Primer commit

---
+++ app/src/main/java/com/albertomoya/readchat/activities/launcher/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.albertomoya.readchat.activities.launcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.albertomoya.readchat.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/com/albertomoya/readchat/activities/login/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.albertomoya.readchat.activities.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.albertomoya.readchat.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
