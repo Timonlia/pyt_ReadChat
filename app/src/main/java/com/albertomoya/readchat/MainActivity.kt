2aa3f2a342437af1b76940d4cbc8a266baab8bf4
Alberto Moya <71229013+Timonlia@users.noreply.github.com>
2020-10-29 09:13:43 +0100

    Provisional NavigationDrawer add setUps NavigationDrawe

--- app/src/main/java/com/albertomoya/readchat/MainActivity.kt
+++ app/src/main/java/com/albertomoya/readchat/MainActivity.kt
@@ -2,12 +2,16 @@ package com.albertomoya.readchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import com.albertomoya.mylibrary.activities.ToolbarActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.*
 
-class MainActivity : ToolbarActivity() {
+class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     // Firebase
         // Autentificador
@@ -16,7 +20,20 @@ class MainActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // SetUps
         toolbarToLoad(toolbar as Toolbar)
+        setNavDrawer()
+    }
 
+    private fun setNavDrawer(){
+        val toogle = ActionBarDrawerToggle(this,drawerLayout, _toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer)
+        toogle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
