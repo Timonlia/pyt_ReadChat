adac05aefa289e960927dcbd68d287d703d7689c
Alberto Moya <71229013+Timonlia@users.noreply.github.com>
2020-11-04 11:10:36 +0100

    Image profile toolbar

--- app/src/main/java/com/albertomoya/readchat/MainActivity.kt
+++ app/src/main/java/com/albertomoya/readchat/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.albertomoya.readchat
 
-import android.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
+
+
+
 import android.os.Bundle
-import android.view.Gravity
+import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import com.albertomoya.mylibrary.activities.ToolbarActivity
 import com.albertomoya.readchat.dialogs.LogoutDialog
 import com.albertomoya.readchat.fragments.*
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.nav_header.*
 import kotlinx.android.synthetic.main.nav_header.view.*
+
 
 class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -49,7 +54,7 @@ class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedL
             R.id.nav_create_book -> fragmentTransaction(AddBookFragment())
             R.id.nav_chats -> fragmentTransaction(ChatFragment())
             R.id.nav_profile -> fragmentTransaction(ProfileFragment())
-            R.id.nav_logout -> LogoutDialog().show(supportFragmentManager,"")
+            R.id.nav_logout -> LogoutDialog().show(supportFragmentManager, "")
         }
     }
 
@@ -62,7 +67,13 @@ class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedL
     }
 
     private fun setNavDrawer(){
-        val toogle = ActionBarDrawerToggle(this,drawerLayout, _toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer)
+        val toogle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            _toolbar,
+            R.string.nav_open_drawer,
+            R.string.nav_close_drawer
+        )
         toogle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(toogle)
         toogle.syncState()
@@ -75,11 +86,43 @@ class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedL
         return true
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar_options, menu)
+
+        val menuItem = menu!!.findItem(R.id.item_profile)
+        // val view: View = MenuItemCompat.getActionView(menuItem)
+        val view: View = menuItem.actionView
+        val profileImage: CircleImageView = view.findViewById(R.id.toolbar_profile_image)
+        val urlPhotoProfile = mAuth.currentUser!!.photoUrl
+        if (urlPhotoProfile != null){
+            Glide
+                .with(this)
+                .load(mAuth.currentUser!!.photoUrl)
+                .into(profileImage)
+        }else{
+            Glide
+                .with(this)
+                .load(R.drawable.ic_person_white)
+                .into(profileImage)
+        }
+
+        profileImage.setOnClickListener { fragmentTransaction(ProfileFragment()) }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_profile -> fragmentTransaction(ProfileFragment())
+            // R.id.menu_three ->
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawer(GravityCompat.START)
         else
-            LogoutDialog().show(supportFragmentManager,"")
+            LogoutDialog().show(supportFragmentManager, "")
         // super.onBackPressed()
     }
 }
